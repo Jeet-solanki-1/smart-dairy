3c510b3a638a03397d0325a7eb887320bfda8528
jeet solanki <solankijeet285@gmail.com>
2025-06-16 14:01:15 +0530

    all done all ghost errors are resolved nothing just i called the mainapp twice inside the app theme

--- app/src/main/java/com/jlss/smartDairy/MainActivity.kt
+++ app/src/main/java/com/jlss/smartDairy/MainActivity.kt
@@ -39,8 +39,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MukeshDairyApp()
-
 
             // Request permissions on launch
                     RequestAudioPermissionIfNeeded()
